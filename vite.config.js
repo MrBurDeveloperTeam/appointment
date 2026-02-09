a97fe5561d85a946815ef0a3fe4db63439e8a9e0
Nizam <leadexpert@mr-bur.com>
2026-02-09 10:23:00 +0800

    add allowed host for cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-﻿import { defineConfig } from 'vite';
+﻿import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+// Fix for __dirname in ESM modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ mode }) => {
+  
+    // Load env variables from the root directory
+    const env = loadEnv(mode, '.', '');
+
+    const isDev = mode === "development";
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+        strictPort: false,
+        allowedHosts: true,
+      }
+    };
 });
