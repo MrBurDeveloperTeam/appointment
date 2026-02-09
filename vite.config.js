1cab61cdbd92ca3f5bd7c9a690bac219f7a12029
KrisLowz <horuslow0218@gmail.com>
2026-02-09 11:27:49 +0800

    chore: Update Vite configuration and regenerate dependency pre-bundles and lock files.

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
-  
-    // Load env variables from the root directory
-    const env = loadEnv(mode, '.', '');
 
-    const isDev = mode === "development";
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-        strictPort: false,
-        allowedHosts: true,
-      }
-    };
+  // Load env variables from the root directory
+  const env = loadEnv(mode, '.', '');
+
+  const isDev = mode === "development";
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      strictPort: false,
+      allowedHosts: true,
+    }
+  };
 });
